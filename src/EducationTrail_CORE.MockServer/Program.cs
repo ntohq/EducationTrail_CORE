5736ef4101b259eb258196903d90da2b8a73cb57
Wesley Ford <wes@wesdev.pro>
2023-06-08 02:53:30 -0400

    Updated Project Structure
    1. Moved Solution File
    2. Fixed Package Sources Issues
    3. Added MongoDB's Driver to Library Project

---
+++ src/EducationTrail_CORE.MockServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationTrail_CORE.MockServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ src/EducationTrail_CORE.MockTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EducationTrail_CORE.MockTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
